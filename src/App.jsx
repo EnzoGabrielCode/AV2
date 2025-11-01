65499114ab5d2fb9a9df7260a69197080fec7cf8
Enzo Gabriel <enzokof2005@gmail.com>
2025-10-31 22:24:43 -0300

    feat: tela de login com tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-function App() {
-  const [message, setMessage] = useState("Olá mundo!");
+import Login from "./components/login";
+import "./App.css"
 
+function App() {
   return (
-    <div>
-      
+    <div className="w-screen h-screen flex justify-center items-center p-6">
+      <Login />
     </div>
   );
 }
---
+++ src/components/login.jsx
@@ -0,0 +1,14 @@
+function Login() {
+    return (
+        <div className="bg-[rgba(58,58,58,0.596)] flex flex-col justify-center items-center rounded-[8%] py-[62px] px-8 h-fit w-fit">
+            <h1 className="text-white font-bold text-center text-[3.2rem] pb-[10%]">AEROCODE</h1>
+            <form className="flex flex-col items-center justify-center gap-[15px] " action="">
+                <input className="bg-gray-100 w-4/5 text-[2rem] text-center rounded-xl" type="text" name="user" id="user" placeholder="USER"/>
+                <input className="bg-gray-100 w-4/5 text-[2rem] text-center rounded-xl" type="password" name="password" id="password" placeholder="SENHA"/>
+                <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-xl">Entrar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
